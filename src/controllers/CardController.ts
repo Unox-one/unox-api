a00c420731f43d1bbe6c6e7ac6721cc31bfdade4
Unox-one <139261343+Unox-one@users.noreply.github.com>
2023-07-31 06:47:09 +0100

    Update CardController.ts

--- src/controllers/CardController.ts
+++ src/controllers/CardController.ts
@@ -20,5 +20,49 @@ export default {
         }
   
         return sendResponse(req, res, 200, "Card request attempt", response);
+    },
+
+    makeMultipleCardRequest: async (req: Request, res: Response)  => {
+      const response = await CardService.makeMultipleCardRequest(req);
+      if (!response) {
+        return sendResponse(req, res, 400, "Card request failed");
+      }
+
+      if (!response.success) {
+        return sendResponse(req, res, 400, "Card request attempt", response);
+      }
+
+      return sendResponse(req, res, 200, "Card request attempt", response);
+    },
+
+    createDelivery: async (req: Request, res: Response)  => {
+      const { error, value } = Validator.validateDeliveryDetails(req.body);
+      if (error) {
+          return sendResponse(req, res, 400, error.details[0].message);
+      }
+      
+      const response = await CardService.createDelivery(req);
+      if (!response) {
+        return sendResponse(req, res, 400, "Create delivery failed");
+      }
+
+      if (!response.success) {
+        return sendResponse(req, res, 400, "Create delivery attempt", response);
+      }
+
+      return sendResponse(req, res, 200, "Create delively attempt", response);
+    },
+
+    createCardProperty: async (req: Request, res: Response)  => {
+      const response = await CardService.createCardProperty(req);
+      if (!response) {
+        return sendResponse(req, res, 400, "Card property creation failed");
+      }
+
+      if (!response.success) {
+        return sendResponse(req, res, 400, "Card property creation attempt", response);
+      }
+
+      return sendResponse(req, res, 200, "Card property creation attempt", response);
     }
 }
