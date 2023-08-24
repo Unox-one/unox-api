f3cbcdc299a0db47b310942c34da3d05cb41dfb8
Unox-one <139261343+Unox-one@users.noreply.github.com>
2023-08-24 16:24:16 +0100

    Update MandrillTemplates.ts

--- src/enums/MandrillTemplates.ts
+++ src/enums/MandrillTemplates.ts
@@ -1,7 +1,8 @@
 const MailTemplates = {
     OTP: "verification-otp",
     RESET_PASSWORD: "reset-password",
-    PASSWORD_RESET_SUCCESS: "password-reset-success"
+    PASSWORD_RESET_SUCCESS: "password-reset-success",
+    WELCOME_MESSAGE: "welcome-email"
 }
 
 export default MailTemplates;
