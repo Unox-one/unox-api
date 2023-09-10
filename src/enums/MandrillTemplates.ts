1f46cf02c4c533e002791eed172ac671833f74b9
Unox-one <139261343+Unox-one@users.noreply.github.com>
2023-09-10 21:51:07 +0100

    Update and rename MandrillTemplates.ts to PostmarkTemplates.ts

--- src/enums/MandrillTemplates.ts
+++
@@ -1,8 +0,0 @@
-const MailTemplates = {
-    OTP: "verification-otp",
-    RESET_PASSWORD: "reset-password",
-    PASSWORD_RESET_SUCCESS: "password-reset-success",
-    WELCOME_MESSAGE: "welcome-email"
-}
-
-export default MailTemplates;
---
+++ src/enums/PostmarkTemplates.ts
@@ -0,0 +1,8 @@
+const MailTemplates = {
+    OTP: 33101990,
+    RESET_PASSWORD: 33107727,
+    PASSWORD_RESET_SUCCESS: 33106599,
+    WELCOME_MESSAGE: 33107726
+}
+
+export default MailTemplates;
